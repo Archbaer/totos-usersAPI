4107f7da827e03a02e478d450c1dd1a0379ca152
archbaer <pereirakebaili@gmail.com>
2025-03-04 10:48:55 +0000

    DTO models done

---
+++ usersAPI/src/main/java/com/usersapi/usersAPI/model/ProductRequest.java
@@ -0,0 +1,62 @@
+package com.usersapi.usersAPI.model;
+
+public class ProductRequest {
+    private String name;
+    private String description;
+    private double price;
+    private int stockQuantity;
+
+    public ProductRequest(String name, String description, double price, int stockQuantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public ProductRequest() {
+
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductRequest{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", stockQuantity=" + stockQuantity +
+                '}';
+    }
+
+}
